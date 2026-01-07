7abeabb9d2af2cb67e87963e59c993753137f307
Vishruth <notvishruth@gmail.com>
2026-01-07 17:38:54 +0530

    Initial commit: Rock Paper Scissors game in C

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int getComputerChoice(){
+
+}
+
+int getPlayerChoice(){
+
+}
+
+void determineWinner(int player, int computer){
+
+}
+
+const char* moveName(int move){
+    
+}
