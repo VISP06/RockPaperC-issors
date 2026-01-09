25ccf7e1db3bd42aecf455d74e16e66f6648f787
Vishruth <notvishruth@gmail.com>
2026-01-09 20:31:30 +0530

    Functioning game but improvements await

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include "rand_gen.h"
 
 int getComputerChoice(){
-    int low = 1;
-    int high = 3;
+    set_seed();
     return randomNumberGenerator(1, 3);
 }
 
@@ -12,24 +12,67 @@ int getPlayerChoice(){
     int choice;
     printf("\n");
     printf("Choose one among the following: \n");
-    printf("------------------------------------------\n");
-    printf("1-Rock\t\t2-Paper\t\t3-Scissors\n");
-    printf("------------------------------------------\n");
+    printf("\t\t------------------------------------------\n");
+    printf("\t\t1-Rock\t\t2-Paper\t\t3-Scissors\n");
+    printf("\t\t------------------------------------------\n");
     printf("Enter in your choice: ");
     scanf("%d", &choice);
+    printf("\n\n");
     return choice;
 }
 
 void determineWinner(int player, int computer){
-
+    if(player==computer){
+        printf("It's a DRAW!!");
+        return;
+    }
+    if(player == 1 && computer == 2){
+        printf("Computer WINS!!");
+        return;
+    }else if(player == 2 && computer == 1){
+        printf("You WIN !!!");
+        return;
+    }
+    if(player == 2 && computer == 3){
+        printf("Computer WINS!!");
+        return;
+    }else if(player == 3 && computer == 2){
+        printf("You WIN!!!");
+        return;
+    }
+    if(player == 3 && computer == 1){
+        printf("You WIN!!!");
+        return;
+    }else if(player == 1 && computer == 3){
+        printf("Computer WINS!!");
+        return;
+    }
 }
 
 const char* moveName(int move){
-    
+    switch(move){
+        case 1 : return "Rock!";
+        case 2 : return "Paper!";
+        case 3 : return "Scissors!";
+        default : printf("Experiencing Technical Difficulties.\n");
+    }
 }
 
 int main(void){
-    int num = getComputerChoice();
-    printf("%d", num);
+    printf("________________________________________________________________________________\n");
+    int playerChoice;
+    do{
+        
+        int computerChoice = getComputerChoice();
+        playerChoice = getPlayerChoice();
+        char * computerMove = moveName(computerChoice);
+        char * playerMove = moveName(playerChoice);
+        printf("You -> \033[1m%s\033[0m\n", playerMove);
+        printf("Computer -> \033[1m%s\033[0m\n", computerMove);
+        printf("\n");
+        determineWinner(playerChoice, computerChoice);
+        printf("\n");
+        printf("________________________________________________________________________________\n");
+    }while(playerChoice != 4);
     return 0;
 }
