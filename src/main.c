4456b9535a2503919e5099f05f04d2c5d98b0e25
Vishruth <notvishruth@gmail.com>
2026-01-10 16:33:26 +0530

    Improved code design of determineWinner func

--- src/main.c
+++ src/main.c
@@ -17,35 +17,31 @@ int getPlayerChoice(){
     printf("\t\t------------------------------------------\n");
     printf("Enter in your choice: ");
     scanf("%d", &choice);
-    printf("\n\n");
+    printf("\n");
     return choice;
 }
-
+//note to self: improve efficiency of this function later by adding nested switch statements
 void determineWinner(int player, int computer){
-    if(player==computer){
-        printf("It's a DRAW!!");
-        return;
-    }
-    if(player == 1 && computer == 2){
-        printf("Computer WINS!!");
-        return;
-    }else if(player == 2 && computer == 1){
-        printf("You WIN !!!");
-        return;
-    }
-    if(player == 2 && computer == 3){
-        printf("Computer WINS!!");
-        return;
-    }else if(player == 3 && computer == 2){
-        printf("You WIN!!!");
-        return;
-    }
-    if(player == 3 && computer == 1){
-        printf("You WIN!!!");
-        return;
-    }else if(player == 1 && computer == 3){
-        printf("Computer WINS!!");
-        return;
+    switch(player){
+        case 1 : 
+            switch(computer){
+                case 1 : printf("It's a DRAW !!"); break;
+                case 2 : printf("Computer WINS !!"); break;
+                case 3 : printf("You WIN!!!"); break;
+            } break;
+        case 2 : 
+            switch(computer){
+                case 1 : printf("You WIN!!!"); break;
+                case 2 : printf("It's a DRAW !!"); break;
+                case 3 : printf("Computer WINS !!"); break;
+            } break;
+        case 3 :
+            switch(computer){
+                case 1 : printf("Computer WINS !!"); break;
+                case 2 : printf("You WIN!!!"); break;
+                case 3 : printf("It's a DRAW !!"); break;
+            } break;
+        default : printf("Please enter a valid option only.");
     }
 }
 
@@ -57,19 +53,21 @@ const char* moveName(int move){
         default : printf("Experiencing Technical Difficulties.\n");
     }
 }
-
+//note to self: can strive for cleaner code by moving the game loop to a separate function
+//add exit button aka fix the exit condition
+//future features: add a way to keep score
 int main(void){
     printf("________________________________________________________________________________\n");
     int playerChoice;
     do{
-        
+
         int computerChoice = getComputerChoice();
         playerChoice = getPlayerChoice();
         char * computerMove = moveName(computerChoice);
         char * playerMove = moveName(playerChoice);
         printf("You -> \033[1m%s\033[0m\n", playerMove);
         printf("Computer -> \033[1m%s\033[0m\n", computerMove);
-        printf("\n");
+        printf("\n");        
         determineWinner(playerChoice, computerChoice);
         printf("\n");
         printf("________________________________________________________________________________\n");
